743fbe2898c0fd564a943234f1150055be0cea54
João Gomes <joaogomes@Joaos-Air.lan>
2024-06-11 23:09:50 +0100

    feat: adds eslint and prettier configs

---
+++ config/eslint-config/node.js
@@ -0,0 +1,8 @@
+/** @type {import('eslint').Linter.Config} */
+module.export = {
+    extends: ['@rocketseat/eslint-config/node'],
+    plugins: ['simple-import-sort'],
+    rules: {
+      'simple-import-sort/imports': "error"
+    }
+  }

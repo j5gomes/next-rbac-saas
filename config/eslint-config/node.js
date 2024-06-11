c4f11415972242237d70d37af4d25f671cc5c3ff
João Gomes <joaogomes@Joaos-Air.lan>
2024-06-11 23:30:37 +0100

    fix: typeo errors

--- config/eslint-config/node.js
+++ config/eslint-config/node.js
@@ -1,8 +1,8 @@
 /** @type {import('eslint').Linter.Config} */
-module.export = {
-    extends: ['@rocketseat/eslint-config/node'],
-    plugins: ['simple-import-sort'],
-    rules: {
-      'simple-import-sort/imports': "error"
-    }
-  }
+module.exports = {
+  extends: ['@rocketseat/eslint-config/node'],
+  plugins: ['simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'error',
+  },
+}

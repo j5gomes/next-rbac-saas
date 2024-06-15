00f79783ed148794a8798910bdeb61b6ed5b08b5
joaogomesdev <joaopfg.2002@gmail.com>
2024-06-15 01:17:22 +0100

    feat: improves permission types with zod

--- packages/auth/src/subjects/project.ts
+++ packages/auth/src/subjects/project.ts
@@ -1,1 +1,15 @@
-export type ProjectSubject = ['create' | 'delete' | 'manage', 'Project']
+import { z } from 'zod'
+
+// tuple is an array with only to positions
+export const projectSubject = z.tuple([
+  z.union([
+    z.literal('manage'),
+    z.literal('get'),
+    z.literal('create'),
+    z.literal('update'),
+    z.literal('delete'),
+  ]),
+  z.literal('Project'),
+])
+
+export type ProjectSubject = z.infer<typeof projectSubject>

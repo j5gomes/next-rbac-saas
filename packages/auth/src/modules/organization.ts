067e127a048cc6567526eb861e7c32aded860716
joaogomesdev <joaopfg.2002@gmail.com>
2025-04-09 02:19:10 +0100

    cleanup comments

--- packages/auth/src/modules/organization.ts
+++ packages/auth/src/modules/organization.ts
@@ -1,8 +1,11 @@
 import { z } from 'zod'
 
+// NOTE: we only need to save the information that is related to permissions, we dont need all database fields here
+// Atributte based authorization control
+
 export const organizationSchema = z.object({
   __typename: z.literal('Organization').default('Organization'),
-  id: z.string(), // Atributte based authorization control
+  id: z.string(),
   ownerId: z.string(),
 })
 
